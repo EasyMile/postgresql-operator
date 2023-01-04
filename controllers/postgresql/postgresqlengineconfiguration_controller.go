d6dca8ac355410ca88201a3cde5395afb23e292c
Alexandre Havrileck <alexandre.havrileck@easymile.com>
2023-01-04 13:57:12 +0100

    fix: Fix RBAC declarations for secrets

--- controllers/postgresql/postgresqlengineconfiguration_controller.go
+++ controllers/postgresql/postgresqlengineconfiguration_controller.go
@@ -51,7 +51,7 @@ type PostgresqlEngineConfigurationReconciler struct { //nolint: golint,revive //
 //+kubebuilder:rbac:groups=postgresql.easymile.com,resources=postgresqlengineconfigurations,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=postgresql.easymile.com,resources=postgresqlengineconfigurations/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=postgresql.easymile.com,resources=postgresqlengineconfigurations/finalizers,verbs=update
-//+kubebuilder:rbac:groups=core,resources=secrets,verbs=get
+//+kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
